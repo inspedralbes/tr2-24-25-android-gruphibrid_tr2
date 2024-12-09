ded0d312e43a35d644680df7d37ae502148dae10
Hasham <a23hashusraf@inspedralbes.cat>
2024-12-09 10:13:57 +0100

    pantalla login

--- GoMath/app/src/main/java/com/example/gomath/ui/GoMathApp.kt
+++ GoMath/app/src/main/java/com/example/gomath/ui/GoMathApp.kt
@@ -1,2 +1,39 @@
 package com.example.gomath.ui
 
+import android.content.Context
+import androidx.annotation.StringRes
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.gomath.MainActivity
+import com.example.gomath.R
+import com.example.gomath.ui.screens.LoginScreen
+
+enum class GoMathApp(@StringRes val title: Int) {
+    Login(title = R.string.login),
+    Code(title = R.string.code),
+    Control(title =R.string.control),
+    Result(title = R.string.result)
+}
+
+@Composable
+fun GoMathApp(navController: NavHostController, context: Context) {
+    val viewModel: GoMathViewModel = viewModel();
+
+    NavHost(navController, startDestination = GoMathApp.Login.name) {
+        composable(route = GoMathApp.Login.name) {
+            LoginScreen()
+        }
+//        composable(route = GoMathApp.Code.name) {
+//            CodeScreen()
+//        }
+//        composable(route = GoMathApp.Control.name) {
+//            ControlScreen()
+//        }
+//        composable(route = GoMathApp.Result.name) {
+//            ResultScreen()
+//        }
+    }
+}
--- GoMath/app/src/main/java/com/example/gomath/ui/GoMathViewModel.kt
+++ GoMath/app/src/main/java/com/example/gomath/ui/GoMathViewModel.kt
@@ -1,2 +1,38 @@
 package com.example.gomath.ui
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class GoMathViewModel() : ViewModel() {
+
+//    private val loginError = mutableStateOf<String?>(null)
+//
+//    fun loginViewModel(email: String, password: String) {
+//        viewModelScope.launch {
+//            val hashedPassword = hashPassword(password)
+//            val loginRequest = LoginRequest(email, hashedPassword)
+//
+//            loginError.value = null
+//
+//            login(loginRequest) { loginResponse, throwable ->
+//                if (throwable != null) {
+//                    Log.e("login", "Error de xarxa: ${throwable.message}")
+//                } else if (loginResponse != null && loginResponse.Confirmacio) {
+//                    // Login successful
+//                    val user = Usuari(
+//                        loginResponse.idUser,
+//                        loginResponse.Nom,
+//                        loginResponse.Correu,
+//                        loginResponse.Contrasenya
+//                    )
+//                    currentUser.value = user
+//                    loginError.value = null
+//                } else {
+//                    loginError.value = "Correu o contrasenya incorrectes"
+//                }
+//            }
+//        }
+//    }
+}
